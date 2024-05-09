be706335f4dc121f8bda398a58417e36989630a8
Luis <lmarrero1070@gmail.com>
2024-05-09 10:14:51 -0400

    Fix output 5-print_comb2.py file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,12 +2,12 @@
 i = 0
 y = 0
 for x in range(99):
+    if x != 98:
+        print("{}{}, " .format(i, y), end="")
     if y == 9:
         i += 1
     if y == 9:
         y = 0
     else:
         y += 1
-    if x != 98:
-        print("{}{}, " .format(i, y), end="")
 print("{}{}" .format(i, y))
