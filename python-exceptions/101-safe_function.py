8680f5b7ddad965517deb63786f5f36fb773fd5a
Luis <lmarrero1070@gmail.com>
2024-05-15 17:18:10 -0400

    Add 101-safe_function.py

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except IndexError as e:
+        print("Exception: {}" .format(e), file=sys.stderr)
+        return None
+    except ZeroDivisionError as e:
+        print("Exception: {}" .format(e), file=sys.stderr)
+        return None
