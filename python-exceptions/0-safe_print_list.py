796974875e82f43c450ff0ab7b79c736a2533393
Luis <lmarrero1070@gmail.com>
2024-05-15 10:36:53 -0400

    Fix handle errors in 0.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    loop = 0
     try:
         for i in range(x):
             print("{:d}" .format(my_list[i]), end="")
+            loop += 1
         print()
-        return i + 1
+        return loop
     except:
         print()
         return i
