711213a4725ae377b99c7dcb910e5c55a461e634
Luis <lmarrero1070@gmail.com>
2024-05-16 13:54:12 -0400

    Add 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """A class used to represent a Square.
+
+    This is an empty class that currently does not have any attributes or methods.
+    It serves as a starting point for creating square-related functionalities.
+    """
+    pass
+
