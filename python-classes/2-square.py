d0b3cd36b05418330a32b26dbd192b13631454eb
Luis <lmarrero1070@gmail.com>
2024-05-16 15:46:48 -0400

    Fix name attribute

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -42,4 +42,4 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
