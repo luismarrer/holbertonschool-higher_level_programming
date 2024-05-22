d48efc08f1eb7c01826024409c8503b8188fd62f
Luis <lmarrero1070@gmail.com>
2024-05-22 18:31:40 -0400

    Update module 7-base_geometry with string format

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,17 +15,8 @@ class BaseGeometry:
         integer_validator(self, name, value): Validates that
         a parameter is a positive integer.
     """
-    def area(self):
-        """
-        Base function.
 
-        Raises:
-            Exception: Always raises an exception with the
-            message "area() is not implemented".
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
+def integer_validator(self, name, value):
         """
         Validates that a parameter is a positive integer.
 
@@ -41,3 +32,13 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+    def area(self):
+        """
+        Base function.
+
+        Raises:
+            Exception: Always raises an exception with the
+            message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
