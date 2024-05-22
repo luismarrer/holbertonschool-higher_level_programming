a7f154d50cc3ab3701de00462832d5fe96e8a03f
Luis <lmarrero1070@gmail.com>
2024-05-21 23:38:45 -0400

    Add documention in module 0-rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines the Rectangle Class.
+Currently, it is an empty class
+serving as a placeholder for future implementation.
+"""
+
 
 class Rectangle:
+    """
+    Rectangle claa
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    None
+    """
     pass
