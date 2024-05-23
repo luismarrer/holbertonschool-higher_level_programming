34498f3ba7f5a243d04c89c73a78a6ad3c7a1bca
Luis <lmarrero1070@gmail.com>
2024-05-23 15:55:14 -0400

    Add documentation in module 100-my_int

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
+This module contains the subclass MyInt of class int.
 """
 
 
 class MyInt(int):
     """
+     A subclass of int that inverts the == and != operators.
     """
-    def __init__(self, value):
-        """
-        """
-        self.value = value
-
     def __eq__(self, other):
         """
+        Inverts the behavior of the equality operator (==).
         """
         return super().__ne__(other)
 
     def __ne__(self, other):
         """
+        Inverts the behavior of the inequality operator (!=).
         """
         return super().__eq__(other)
