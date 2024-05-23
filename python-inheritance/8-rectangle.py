f069d6db59737ecebf3ecd7bf63be2c9b5368f4e
Luis <lmarrero1070@gmail.com>
2024-05-22 22:39:24 -0400

    fix exercise 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -62,6 +62,6 @@ class Rectangle(BaseGeometry):
             validated as a positive integer.
         """
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-        self.__width = width
