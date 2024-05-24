bce2f1d9b6d9578532782b46baf6ecacde0b5d6a
Luis <lmarrero1070@gmail.com>
2024-05-23 22:32:53 -0400

    Add exercise 13

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def add_attribute(obj, attribute, value):
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attribute, value)
