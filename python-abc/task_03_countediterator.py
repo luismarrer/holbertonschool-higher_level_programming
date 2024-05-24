89f2df9c239a76f7ed17791e7b6d61eddbd57fd8
Luis <lmarrero1070@gmail.com>
2024-05-24 13:28:00 -0400

    Add exercise 3

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,32 @@
+class CountedIterator:
+    """
+    CountedIterator extends the built-in iterator to keep track
+    of the number of items iterated over.
+    """
+    def __init__(self, iterable):
+        """
+        Initialize the CountedIterator with an iterable.
+        """
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        """
+        Return the next item from the iterator and increment the counter.
+        Raise StopIteration when there are no more items.
+        """
+        item = next(self.iterator)  # This will raise StopIteration if no items are left
+        self.count += 1
+        return item
+
+    def get_count(self):
+        """
+        Return the number of items that have been iterated over.
+        """
+        return self.count
+
+    def __iter__(self):
+        """
+        Return the iterator itself.
+        """
+        return self
