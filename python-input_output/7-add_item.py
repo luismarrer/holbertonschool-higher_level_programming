dc856c94c4a696a60921ee0072ef483cf2d1f6d5
Luis <lmarrero1070@gmail.com>
2024-05-28 14:12:59 -0400

    Add exercise 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contain a script hat adds
+all arguments to a Python list, and then save them to a file:
+"""
+
+
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    my_list = load_from_json_file('add_item.json')
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, 'add_item.json')
+except Exception:
+    my_list = sys.argv[1:]
+    save_to_json_file(my_list, 'add_item.json')
