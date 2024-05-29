046f05fff44ba9df527d6157d15edd6957f5e74d
Luis <lmarrero1070@gmail.com>
2024-05-29 13:47:21 -0400

    Add exercise 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,18 +1,22 @@
 import json
 
+
 def serialize_and_save_to_file(data, filename):
     """
     Serialize a Python dictionary to a JSON file.
 
     :param data: A Python dictionary with data.
-    :param filename: The filename of the output JSON file. If the file already exists, it should be replaced.
+    :param filename: The filename of the output JSON file.
+    If the file already exists, it should be replaced.
     """
     with open(filename, 'w') as file:
         json.dump(data, file)
 
+
 def load_and_deserialize(filename):
     """
-    Load and deserialize data from a JSON file to a Python dictionary.
+    Load and deserialize data from a JSON
+    file to a Python dictionary.
 
     :param filename: The filename of the input JSON file.
     :return: A Python dictionary with the deserialized data from the file.
