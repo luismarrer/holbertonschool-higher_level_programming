5cd58e68250de423885ae119241484e0351a7427
Luis <lmarrero1070@gmail.com>
2024-05-29 14:09:18 -0400

    Add exercise 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python3
+"""
+This module provides functionalities to serialize and deserialize
+custom Python objects using the pickle module.
+
+The module defines a CustomObject class with the following attributes:
+- name: A string representing the name of the object.
+- age: An integer representing the age of the object.
+- is_student: A boolean indicating whether the object represents a student.
+
+The CustomObject class includes methods for displaying the object's attributes,
+serializing the object to a file, and deserializing the object from a file.
+"""
 
 
 import pickle
 
 
 class CustomObject:
+    """
+    A custom Python class representing an object
+    with attributes name, age, and is_student.
+
+    Attributes:
+    - name (str): The name of the object.
+    - age (int): The age of the object.
+    - is_student (bool): Indicates if the object represents a student.
+
+    Methods:
+    - display(): Prints the attributes of the object in a formatted string.
+    - serialize(filename): Serializes the object to the specified filename.
+    - deserialize(cls, filename): Class method to
+    deserialize an object from the specified filename.
+    """
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
