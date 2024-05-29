abd0f2fad708e5e82d768f47b1c33176c4d8640b
Luis <lmarrero1070@gmail.com>
2024-05-28 20:42:01 -0400

    Add exercise 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module defines the Student class,
+which represents a student
+with attributes for the first name, last name, and age.
+"""
+
+
+class Student:
+    """
+    Represents a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__json__()
