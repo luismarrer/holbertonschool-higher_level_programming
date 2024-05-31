08a921ec5b371fde21915ceb762ff3cddd079d73
Luis <lmarrero1070@gmail.com>
2024-05-31 03:35:39 -0400

    Add exercise 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,10 +3,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {
-        "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-        "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-        }
+users = {}
 
 @app.route('/')
 def home():
