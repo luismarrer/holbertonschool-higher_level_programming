fb03c94fb4290d0cf8dcb65c82d6f18a255beb46
Luis <lmarrero1070@gmail.com>
2024-05-31 02:26:02 -0400

    Add exercise 4

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {
+        "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+        "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+        }
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route('/data')
+def get_data():
+    return jsonify(list(users.key()))
+
+@app.route('/status')
+def status():
+    return "OK"
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    new_user = request.get_json()
+    username = new_user.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    if username in users:
+        return jsonify({"error": "User already exists"}), 400
+    users[username] = new_user
+    return jsonify({"message": "User added", "user": new_user}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
