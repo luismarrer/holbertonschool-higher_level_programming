5b94673a46b77261b4ec9217ec7902b591dad1b0
Luis <lmarrero1070@gmail.com>
2024-05-31 01:42:12 -0400

    Add exercise 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,6 +31,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                     }
             self.wfile.write(json.dumps(status).encode())
 
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.wfile.write(json.dumps(info).encode())
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
