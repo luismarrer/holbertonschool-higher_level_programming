17e4be87e1d8db6775b438977296d03d09a32499
Luis <lmarrero1070@gmail.com>
2024-05-31 00:21:22 -0400

    Add exercise 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import http.server
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {
+                    "name": "John",
+                    "age": 30,
+                    "city": "New York"
+                    }
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            status = {
+                    "status": "OK"
+                    }
+            self.wfile.write(json.dumps(status).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_message = {
+                    "error": "Endpoint not found"
+                    }
+            self.wfile.write(json.dumps(error_message).encode())
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('',port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
