3c0dc38e482dd51edd0369f25cba35075948e4a5
Luismarrer <lmarrero1070@gmail.com>
2024-06-20 15:24:13 -0400

    Add 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute(f"""SELECT * FROM states
-                WHERE name='{sys.argv[4]}' ORDER BY id ASC""")
+    cur.execute("""SELECT * FROM states
+                WHERE name='{}' ORDER BY id ASC""".format(sys.argv[4]))
     # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
