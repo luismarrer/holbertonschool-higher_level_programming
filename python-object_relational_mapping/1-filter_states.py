3fc323f7051489ba42cab7afb53b6c786d4572df
Luismarrer <lmarrero1070@gmail.com>
2024-06-20 14:34:06 -0400

    Add 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,11 +13,10 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
-        if 'N' in row[1]:
-            print(row)
+        print(row)
     cur.close()
     conn.close()
