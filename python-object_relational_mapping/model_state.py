c22c293646f501ecbadb91d9ff6143dd4be70367
Luismarrer <lmarrero1070@gmail.com>
2024-06-21 11:55:24 -0400

    Add 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+This module defines the State class for the ORM.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,6 +9,7 @@ Base = declarative_base()
 
 class State(Base):
     """
+    This class represents a state in the database.
     """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True,
